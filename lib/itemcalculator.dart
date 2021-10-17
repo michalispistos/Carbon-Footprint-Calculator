e120651898f796c32bf41d26e5816f90297118f6
unknown <mp2019@ic.ac.uk>
2021-10-17 01:50:39 +0100

    added add button where users can add the contents(in terms of %) of their clothes

--- lib/itemcalculator.dart
+++ lib/itemcalculator.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     Color color = Theme.of(context).primaryColor;
 
+
     Widget inputSections = Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
