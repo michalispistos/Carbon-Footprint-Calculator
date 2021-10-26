64f67183477f237783c9cc6d4537b6284cae287a
mipi2610 <michalis.pistos19@imperial.ac.uk>
2021-10-26 22:41:56 +0100

    [CF3YSP-16] Implemented barcode scanning

--- lib/data/item.dart
+++ lib/data/item.dart
@@ -8,15 +8,23 @@ class Item {
   // TODO barcode - store as id? use external package?
   double score = 0;
 
+  String name = "";
+
   // All the doubles in materials should sum to 1 - currently this is a pre
   // condition assumed here
   Item(this.materials, this.type, this.weight);
+
+  factory Item.fromJson(Map<String, dynamic> json) {
+    return Item(json['materials'], json['type'], json['weight'],);
+  }
 
   Item.itemWithScore(Item item, this.score):
     materials = item.materials,
     type = item.type,
     weight = item.weight
   ;
+
+
 
 }
 
