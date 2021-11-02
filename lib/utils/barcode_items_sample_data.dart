2f12b621f10525dfaaa40755d927bd4d014e3f5e
mipi2610 <michalis.pistos19@imperial.ac.uk>
2021-11-02 01:36:51 +0000

    updated sample

--- lib/utils/barcode_items_sample_data.dart
+++ lib/utils/barcode_items_sample_data.dart
@@ -4,12 +4,12 @@ class BarcodeItems {
 
   static final Map<String, Item> sampleBarcodeItems =
   {
-    "062606": Item.itemWithImageAndName(Item({"Cotton": 75,"Polyester": 25}, "Bottom",0.7), 'images/imperial_sweatpants.png',"Imperial Sweatpants"),
-    "062336": Item.itemWithImageAndName(Item({"Cotton": 65,"Polyester": 35}, "Top", 0.4), 'images/imperial_hoody.png', "Imperial Hoody"),
-    "062431": Item.itemWithImageAndName(Item({"Cotton": 80,"Polyester": 20}, "Top", 0.3), 'images/imperial_sweater.png', "Imperial Sweater"),
-    "28003472": Item.itemWithImageAndName(Item({"Polyester": 100}, "Top",1.1), 'images/imperial_jacket.png',"Imperial Jacket"),
-    "062526": Item.itemWithImageAndName(Item({"Cotton": 100}, "Top", 0.25), 'images/imperial_t-shirt.png', "Imperial T-shirt"),
+    "062606": Item.itemWithImageAndName(Item({"Cotton": 75,"Polyester": 25}, "Bottoms",0.7), 'images/imperial_sweatpants.png',"Imperial Sweatpants"),
+    "062336": Item.itemWithImageAndName(Item({"Cotton": 65,"Polyester": 35}, "Tops", 0.4), 'images/imperial_hoody.png', "Imperial Hoody"),
+    "062431": Item.itemWithImageAndName(Item({"Cotton": 80,"Polyester": 20}, "Tops", 0.3), 'images/imperial_sweater.png', "Imperial Sweater"),
+    "28003472": Item.itemWithImageAndName(Item({"Polyester": 100}, "Tops",1.1), 'images/imperial_jacket.png',"Imperial Jacket"),
+    "062526": Item.itemWithImageAndName(Item({"Cotton": 100}, "Tops", 0.25), 'images/imperial_t-shirt.png', "Imperial T-shirt"),
 
-    "5000207007159": Item.itemWithImageAndName(Item({"Acrylic": 100}, "Top", 34.5), 'images/northface-logo.png', "North"),
+    "5000207007159": Item.itemWithImageAndName(Item({"Acrylic": 100}, "Tops", 34.5), 'images/northface-logo.png', "North"),
   };
 }
